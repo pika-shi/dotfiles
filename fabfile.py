11d7c212391680a1f28a8bf6b14ce7c2ffb398c0
Takemura Hikaru <takemura@dl.kuis.kyoto-u.ac.jp>
2014-09-20 16:40:01 +0900

    update fabfile

--- fabfile.py
+++ fabfile.py
@@ -2,28 +2,29 @@
 
 from fabric.api import env, settings
 from fabric.decorators import task
-from cuisine import mode_sudo, select_package, run, package_ensure, dir_exists, cd
+from cuisine import mode_sudo, select_package, run, sudo, package_ensure, dir_exists, cd
 
 env.warn_only = True
-env.hosts = ['hoge']
-select_package('apt')
+env.use_ssh_config = True
 
 @task
-def main():
-  setup_packages()
-  install_go()
-  install_pip()
-  fetch_dotfiles()
+def main(package='apt', git=0):
+  setup_packages(package)
+  fetch_dotfiles(git)
   set_symlinks()
   change_shell()
   complete_brew()
+  install_go()
+  install_pip()
 
 @task
-def setup_packages():
+def setup_packages(package):
+  select_package(package)
   with settings(mode_sudo()):
-    run('apt-get update')
-    run('apt-get -y upgrade')
+    run('{0} -y update'.format(package))
+    run('{0} -y upgrade'.format(package))
     package_ensure('ag')
+    package_ensure('jq')
     package_ensure('tig')
     package_ensure('dfc')
     package_ensure('zsh')
@@ -39,59 +40,64 @@ def setup_packages():
 
 @task
 def install_go():
-  run('wget https://go.googlecode.com/files/go1.2rc2.linux-amd64.tar.gz')
-  with settings(mode_sudo()):
-    run('tar zxvf go1.2rc2.linux-amd64.tar.gz -C /usr/local')
-    run('export PATH=$PATH:/usr/local/go/bin')
-    run('go get github.com/peco/peco/cmd/peco')
-    run('go get github.com/motemen/ghq')
+  if not dir_exists('/usr/local/go'):
+    run('wget https://go.googlecode.com/files/go1.2rc2.linux-amd64.tar.gz')
+    with settings(mode_sudo()):
+      run('tar zxvf go1.2rc2.linux-amd64.tar.gz -C /usr/local')
+      run('export PATH=$PATH:/usr/local/go/bin')
+      run('go get github.com/peco/peco/cmd/peco')
+      run('go get github.com/motemen/ghq')
 
 @task
 def install_pip():
   run('wget https://bootstrap.pypa.io/get-pip.py')
-  with setting(mode_sudo()):
+  with settings(mode_sudo()):
     run('python get-pip.py')
     run('rm get-pip.py')
     run('pip install ipython')
     run('pip install Pygments')
 
 @task
-def fetch_dotfiles():
-  run('wget https://github.com/pika-shi/dotfiles/archive/master.zip')
+def fetch_dotfiles(git):
   if not dir_exists('dotfiles'):
-    run('unzip master.zip')
-    run('rm master.zip')
-    run('mv dotfiles-master dotfiles')
+    if git:
+      run('git clone --recursive git@github.com:pika-shi/dotfiles.git')
+    else:
+      run('wget https://github.com/pika-shi/dotfiles/archive/master.zip')
+      run('unzip master.zip')
+      run('rm master.zip')
+      run('mv dotfiles-master dotfiles')
 
 @task
 def set_symlinks():
-  with cd('dotfiles'):
-    path_list = [('_zshrc', '~/.zshrc'),
-                 ('_zshenv', '~/.zshenv'),
-                 ('_tmux.conf', '~/.tmux.conf'),
-                 ('_vimrc', '~/.vimrc'),
-                 ('_vim', '~/.vim'),
-                 ('_gitignore', '~/.gitignore'),
-                 ('_gitconfig', '~/.gitconfig'),
-                 ('_gitattributes', '~/.gitattributes')]
-  if 'localhost' in env.hosts:
-    path_list.append(('_zshrc.osx', '~/.zshrc.osx'))
-    path_list.append(('_vimperatorrc', '~/.vimperatorrc'))
-    path_list.append(('karabiner.xml',
-                      '~/Library/Application Support/Karabiner/private.xml'))
+  with cd('~/'):
+    path_list = [('dotfiles/_zshrc', '.zshrc'),
+                 ('dotfiles/_zshenv', '.zshenv'),
+                 ('dotfiles/_tmux.conf', '.tmux.conf'),
+                 ('dotfiles/_vimrc', '.vimrc'),
+                 ('dotfiles/_vim', '.vim'),
+                 ('dotfiles/_gitignore', '.gitignore'),
+                 ('dotfiles/_gitconfig', '.gitconfig'),
+                 ('dotfiles/_gitattributes', '.gitattributes')]
+  if run('uname') == 'Darwin':
+    path_list.append(('dotfiles/_zshrc.osx', '.zshrc.osx'))
+    path_list.append(('dotfiles/_vimperatorrc', '.vimperatorrc'))
+    path_list.append(('dotfiles/karabiner.xml',
+                      'Library/Application Support/Karabiner/private.xml'))
     path_list.append(('/usr/local/Library/Contributions/brew_zsh_completion.zsh',
                       '/usr/local/share/zsh/site-functions/_brew'))
 
   with cd('~/'):
     for path_set in path_list:
-      run('ln -sf %s %s' % (path_set[0], path_set[1]))
+      run('ln -sf {0} {1}'.format(path_set[0], path_set[1]))
 
 @task
 def change_shell():
-  run('sed -i "s/^\(.*pam_shells.so$\)/#\1/" /etc/pam.d/chsh')
-  run('zsh')
+  with cd('~/'):
+    sudo('sed -i "s/^\(.*pam_shells.so$\)/#\1/" /etc/pam.d/chsh')
 
 @task
 def complete_brew():
-  with cd('/usr/local/share/zsh/site-functions'):
-    run('ln -sf ../../../Library/Contributions/brew_zsh_completion.zsh _brew')
+  if run('uname') == 'Darwin':
+    with cd('/usr/local/share/zsh/site-functions'):
+      run('ln -sf ../../../Library/Contributions/brew_zsh_completion.zsh _brew')
