e5bab59500b4ef915dce7b22c3283b739ec2da57
Takemura Hikaru <takemura@dl.kuis.kyoto-u.ac.jp>
2014-09-20 17:31:01 +0900

    update fabfile

--- fabfile.py
+++ fabfile.py
@@ -79,21 +79,19 @@ def set_symlinks():
                  ('dotfiles/_gitignore', '.gitignore'),
                  ('dotfiles/_gitconfig', '.gitconfig'),
                  ('dotfiles/_gitattributes', '.gitattributes')]
-  if run('uname') == 'Darwin':
-    path_list.append(('dotfiles/_zshrc.osx', '.zshrc.osx'))
-    path_list.append(('dotfiles/_vimperatorrc', '.vimperatorrc'))
-    path_list.append(('dotfiles/karabiner.xml',
-                      'Library/Application Support/Karabiner/private.xml'))
-    path_list.append(('/usr/local/Library/Contributions/brew_zsh_completion.zsh',
-                      '/usr/local/share/zsh/site-functions/_brew'))
+    if run('uname') == 'Darwin':
+      path_list.append(('dotfiles/_zshrc.osx', '.zshrc.osx'))
+      path_list.append(('dotfiles/_vimperatorrc', '.vimperatorrc'))
+      path_list.append(('dotfiles/karabiner.xml',
+                        'Library/Application Support/Karabiner/private.xml'))
+      path_list.append(('/usr/local/Library/Contributions/brew_zsh_completion.zsh',
+                        '/usr/local/share/zsh/site-functions/_brew'))
 
-  with cd('~/'):
     for path_set in path_list:
       run('ln -sf {0} {1}'.format(path_set[0], path_set[1]))
 
 @task
 def change_shell():
-  with cd('~/'):
     sudo('sed -i "s/^\(.*pam_shells.so$\)/#\1/" /etc/pam.d/chsh')
 
 @task
